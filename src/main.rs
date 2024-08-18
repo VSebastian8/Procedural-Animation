2a1b913092e86d550e819c888f256576cfe1fc15
VSebastian8 <visan-draghicescu.emanuel-sebastian@s.unibuc.ro>
2024-08-19 00:23:16 +0300

    Animating a Circle

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,42 @@
 use iced::{
     mouse,
-    widget::{
-        canvas::{Frame, Geometry, Path, Program},
-        Canvas,
+    executor,
+    time::{
+        self, Duration
     },
-    Color, Length, Point, Rectangle, Renderer, Sandbox, Settings, Theme,
+    widget::{ 
+        canvas::{Frame, Geometry, Path, Program},
+        Canvas
+    },
+    Application, Command, Settings, Color, Length, Point, Rectangle, Theme, Renderer, Vector
 };
 
 fn main() -> iced::Result {
-    AnimationApp::run(Settings::default())
+    MyApp::run(Settings::default())
 }
 
-struct AnimationApp;
+#[derive(Debug, Clone)]
+enum MyAppMessage {
+    Update,
+}
 
-impl Sandbox for AnimationApp {
-    type Message = ();
+struct MyApp {
+    time_units: u32,
+    canvas_state: CanvasState,
+}
 
-    fn new() -> Self {
-        Self
-    }
+struct CanvasState {
+    position: Vector,
+}
 
-    fn title(&self) -> String {
-        String::from("Animal Procedural Animation")
-    }
-
-    fn update(&mut self, _message: Self::Message) {}
-
-    fn view(&self) -> iced::Element<Self::Message> { 
-            Canvas::new(AnimationProgram).width(Length::Fill).height(Length::Fill).into()
+impl CanvasState {
+    pub fn update_state(&mut self){
+        self.position.x += 15.0;
     }
 }
 
-struct AnimationProgram;
-
-impl<Message> Program<Message> for AnimationProgram {
-    type State = ();
+impl<Message> Program<Message> for CanvasState {
+    type State = (); // extra type we do not use, different from CanvasState
 
     fn draw(
         &self,
@@ -46,17 +48,53 @@ impl<Message> Program<Message> for AnimationProgram {
     ) -> Vec<Geometry> {
         let mut frame = Frame::new(renderer, bounds.size());
 
-        // println!("{}", frame.center());
-
         // Drawing the background
         frame.fill_rectangle(Point::ORIGIN, bounds.size(), Color::from_rgb8(2, 2, 32));
 
-        // Drawing a circle of radius 250 in the center of the canvas
+        // Drawing a circle of radius 250 at it's (x, y) position
         frame.fill(
-            &Path::circle(frame.center(), 250.0),
+            &Path::circle(frame.center() + self.position, 250.0),
             Color::from_rgb8(0, 179, 134),
         );
 
         vec![frame.into_geometry()]
+    }
+}
+ 
+impl Application for MyApp {
+    type Executor = executor::Default;
+    type Message = MyAppMessage;
+    type Theme = iced::Theme;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
+        (Self {
+            time_units: 0, 
+            canvas_state: CanvasState {
+                position: [0.0, 0.0].into()}, }, 
+        Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Procedural Animation")
+    }
+
+    fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
+        match message {
+            MyAppMessage::Update => {
+                self.time_units += 1;
+                println!("{}", self.time_units);
+                self.canvas_state.update_state();
+            }
+        }
+        Command::none()
+    }
+
+    fn view(&self) -> iced::Element<Self::Message> {
+        Canvas::new(&self.canvas_state).width(Length::Fill).height(Length::Fill).into()
+    }
+
+    fn subscription(&self) -> iced::Subscription<Self::Message> {
+        time::every(Duration::from_secs(1)).map(|_| MyAppMessage::Update)
     }
 }
